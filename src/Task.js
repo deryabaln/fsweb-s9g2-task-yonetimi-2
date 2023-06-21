e2cd711c7ab66809e20b930ddd097d1e948ab338
derya <deryabalin@gmail.com>
2023-06-21 23:36:56 +0300

    proje tamamlandı

--- src/Task.js
+++ src/Task.js
@@ -19,8 +19,8 @@ const Task = ({ taskObj, onComplete }) => {
   return (
     <div className="p-6 bg-[#fff] rounded-[5px] leading-6 mt-4 shadow-[0_4px_5px_0_rgba(0,0,0,0.1)] ">
       <h3 className="text-lg text-[#c8781a]">{taskObj.title}</h3>
-      <div className="text-xs pt-1">son teslim: <span className={isDeadlineClose? "bg-[#ffd9d4] p-1" : "bg-[none] p-1" } >{distance}</span></div>
-      <p className="text-xs pt-2 pb-2 text-[#444]" >{taskObj.description}</p>
+      <div className="text-sm pt-1">son teslim: <span className={isDeadlineClose? "bg-[#ffd9d4] p-1" : "bg-[none] p-1" } >{distance}</span></div>
+      <p className="text-sm pt-2 pb-2 text-[#444]" >{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
           <span className="inline-block py-1 px-3 text-xs mr-1 mb-1.5 rounded-3xl border-solid border-[1px] border-zinc-300" key={p}>{p}</span>
